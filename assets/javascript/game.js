e1e8eaf4a047d40e504055e50dce7e5c8f147972
rsuttles58 <robert.suttles58@gmail.com>
2019-11-07 10:51:26 -0600

    Significant code refactor.  Additionally, added functionality to not count guesses for already used letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,126 @@
 //*************VARIABLES*************/
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-    "u", "v", "w", "x", "y", "z"];
+var letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
 var theLetter;
 var guessesRemaining = 8;
 var winCounter = 0;
 var lossCounter = 0;
-var userInput;
 var lettersGuessed = [];
-var laugh = new Audio(src="assets/audio/evilvillain.wav")
 
-//*****SCREEN UPDATING FUNCTIONS******* */
+//*****DOM UPDATING FUNCTIONS********/
 function printGuesses() {
-    document.getElementById("guessesLeft").innerHTML = "Guesses Remaining: " + guessesRemaining.toString();
+  document.getElementById("guessesLeft").innerHTML =
+    "Guesses Remaining: " + guessesRemaining.toString();
 }
 
 function printWin() {
-    document.getElementById("wins").innerHTML = "Wins: " + winCounter.toString();
+  document.getElementById("wins").innerHTML = "Wins: " + winCounter.toString();
 }
 
 function printLoss() {
-    document.getElementById("loss").innerHTML = "Losses: " + lossCounter.toString();
+  document.getElementById("loss").innerHTML =
+    "Losses: " + lossCounter.toString();
 }
 
 function printLetters() {
-    document.getElementById("lettersChosen").innerHTML = "Letters Guessed: " + lettersGuessed;
+  document.getElementById("lettersChosen").innerHTML =
+    "Letters Guessed: " + lettersGuessed;
 }
 
 //************BACK END FUNCTIONS************** */
+//newLetter will select a random letter from the array of letters.  Letter will be logged to the console.
 function newLetter() {
-    theLetter = letters[Math.floor(Math.random() * letters.length)];
-    console.log(theLetter);
+  theLetter = letters[Math.floor(Math.random() * letters.length)];
+  console.log(`Current letter: ${theLetter}`);
 }
 
-function theGuess() {
-    if (userInput === theLetter) {
-        winCounter++;
-        lettersGuessed.length = 0;
-        guessesRemaining = 8;
-        newLetter();
-        printWin();
-        printLetters();
-        printGuesses();
-
-        if (winCounter === 1) {
-            laugh.play();
-        } else if (winCounter === 3){
-            alert("You are the champion!");
-        }
-    }
-     else {
-        guessesRemaining--;
-        printGuesses();
-        printLetters();
-        }
-
-    if (guessesRemaining === 0) {
-        lossCounter++;
-        lettersGuessed.length = 0;
-        guessesRemaining = 8;
-        newLetter();
-        printLoss();
-        printGuesses();
-        printLetters();
-    }
-}
-
-function setup() {
+//Processes the user's input (guess)
+function theGuess(userInput) {
+  //if the user guessed correctly....
+  if (userInput === theLetter) {
+    winCounter++;
+    lettersGuessed.length = 0;
+    guessesRemaining = 8;
     newLetter();
     printWin();
+    printLetters();
+    printGuesses();
+    winCheck();
+    //if the user guessed incorrectly and that letter has not already been guessed....
+  } else if (lettersGuessed.includes(userInput) === false) {
+    console.log(`User entered ${userInput}`);
+    lettersGuessed.push(userInput);
+    guessesRemaining--;
     printGuesses();
     printLetters();
+  }
+  gameCheck();
+}
+
+function winCheck() {
+  var laugh = new Audio((src = "assets/audio/evilvillain.wav"));
+
+  if (winCounter === 1) {
+    laugh.play();
+  } else if (winCounter === 3) {
+    alert("You are the champion!");
+  }
+}
+
+//Checks if the user is out of guesses for the letter.  If they are, then it resets the game and adds a loss.
+function gameCheck() {
+  if (guessesRemaining === 0) {
+    lossCounter++;
+    lettersGuessed.length = 0;
+    guessesRemaining = 8;
+    newLetter();
     printLoss();
+    printGuesses();
+    printLetters();
+  }
+}
+//declare the setup function that invokes the processes that reset the game state.
+function setup() {
+  newLetter();
+  printWin();
+  printGuesses();
+  printLetters();
+  printLoss();
 }
 
 //*******GAME PROCESS****************************//
+//Invoke the setup function to start the game.
 setup();
 
-document.onkeyup = function (event) {
-    userInput = event.key.toLowerCase();
-    lettersGuessed.push(userInput);
-    console.log(userInput);
-    theGuess();
-}
-
+//event listener that listens for a key to be released.
+document.onkeyup = function(event) {
+  var userInput = event.key.toLowerCase();
+  theGuess(userInput);
+};
